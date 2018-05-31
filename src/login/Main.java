5db92782137a50f677668dd17d6d106e75f2298f
Pop <popma@media-saturn.com>
2018-05-31 16:09:41 +0300

    withdraw final

--- src/login/Main.java
+++ src/login/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		new Widhraw();
+		new Login();
 	}
 	public static void main(String[] args) {
 	
